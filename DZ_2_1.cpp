4e4fdcdeebac62c0068021d362c933dc9e454e28
Vd_Alexey_Vl <alexey28102@gmail.com>
2026-01-19 21:29:03 +0300

    Добавил русский язык.

--- DZ_2_1.cpp
+++ DZ_2_1.cpp
@@ -1,12 +1,15 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 int main()
 
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     int num;
-    std::cout << "Enter your number: \n";
+    std::cout << " Введите число: \n";
     std::cin >> num;
-    std::cout << "You entered: \n" << num << std::endl;
+    std::cout << "Вы ввели: \n" << num << std::endl;
 
     return EXIT_SUCCESS;
 }
